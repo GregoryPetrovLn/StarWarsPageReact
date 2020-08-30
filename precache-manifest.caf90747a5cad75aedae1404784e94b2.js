cd8182237431a5ebc747474d2cb17da1bd1f0dcd
Gregory <grishap96123@gmail.com>
2020-08-30 16:24:41 +0300

    Updates

---
+++ precache-manifest.caf90747a5cad75aedae1404784e94b2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17d4a7ae308ede68e4df71d9d1ffa7c",
+    "url": "/StarWarsPageReact/index.html"
+  },
+  {
+    "revision": "5a29fbed95edcf90f31c",
+    "url": "/StarWarsPageReact/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "95953e5a1ad368714118",
+    "url": "/StarWarsPageReact/static/css/main.bb74d596.chunk.css"
+  },
+  {
+    "revision": "5a29fbed95edcf90f31c",
+    "url": "/StarWarsPageReact/static/js/2.d219fff3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/StarWarsPageReact/static/js/2.d219fff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95953e5a1ad368714118",
+    "url": "/StarWarsPageReact/static/js/main.8faf5263.chunk.js"
+  },
+  {
+    "revision": "e1301debf0a2856079c6",
+    "url": "/StarWarsPageReact/static/js/runtime-main.c6c118d3.js"
+  },
+  {
+    "revision": "ac447590681568fa253906a4f359ff4e",
+    "url": "/StarWarsPageReact/static/media/friend0.ac447590.jpg"
+  },
+  {
+    "revision": "ddbb6c95abb84f38d457bbcbda1092ca",
+    "url": "/StarWarsPageReact/static/media/friend1.ddbb6c95.jpg"
+  },
+  {
+    "revision": "97f0c7be4a155f2201a247b78601cd81",
+    "url": "/StarWarsPageReact/static/media/friend2.97f0c7be.jpg"
+  },
+  {
+    "revision": "b137b39817e483b0c2bed80f94de1713",
+    "url": "/StarWarsPageReact/static/media/friend3.b137b398.jpg"
+  },
+  {
+    "revision": "1f9bc1b259c641aa7d34be25265a8b70",
+    "url": "/StarWarsPageReact/static/media/friend4.1f9bc1b2.jpg"
+  },
+  {
+    "revision": "32d6eeb58ccef9d20e0b90692fa27805",
+    "url": "/StarWarsPageReact/static/media/friend5.32d6eeb5.jpg"
+  },
+  {
+    "revision": "1ad67883650dd5f8966014b096472a23",
+    "url": "/StarWarsPageReact/static/media/friend6.1ad67883.jpg"
+  },
+  {
+    "revision": "6912308bd9f6699ad5715ac9567c3120",
+    "url": "/StarWarsPageReact/static/media/friend7.6912308b.jpg"
+  },
+  {
+    "revision": "879d7419d30b9d5923b7f75a3a6cb43b",
+    "url": "/StarWarsPageReact/static/media/friend8.879d7419.jpg"
+  },
+  {
+    "revision": "7b4bb470008beab21299e31a6eb303f0",
+    "url": "/StarWarsPageReact/static/media/friend9.7b4bb470.jpg"
+  },
+  {
+    "revision": "83bd4e24ef8e3d5d3d181aba314ae17d",
+    "url": "/StarWarsPageReact/static/media/sky.83bd4e24.jpg"
+  }
+]);
